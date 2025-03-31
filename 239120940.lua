df4f66dda689dee18520ef4de54e06a27ab1bd5a
Wx <106684512+xOne2@users.noreply.github.com>
2025-04-01 00:10:42 +0700

    Update HWID keys

--- 239120940.lua
+++ 239120940.lua
@@ -1,3 +1,5 @@
 local whitelistPak = {
-    { Hwid = "x", Key = "x"{ Hwid = "z", Key = "z"{ Hwid = "asdasdasd", Key = "asdasd"},
+    { Hwid = "x", Key = "x"{ Hwid = "z", Key = "z"{ Hwid = "asdasdasd", Key = "asdasd"   
+s { Hwid = "sdd", Key = "add"},
+    },
   },},}
