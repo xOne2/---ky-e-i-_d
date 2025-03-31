e83323575f5230446bafca814c82e31f5b43d679
Wx <106684512+xOne2@users.noreply.github.com>
2025-03-31 21:29:17 +0700

    Update HWID keys

--- 239120940.lua
+++ 239120940.lua
@@ -1,3 +1,5 @@
 local whitelistPak = {
     { Hwid = "444", Key = "222", ExpiryDate = math.huge, Permanent = true },
+    { Hwid = "hhrhrthrth2233", Key = "2131241241241", ExpiryDate = math.huge, Permanent = true },
+    { Hwid = "", Key = "", ExpiryDate = math.huge, Permanent = true },
 }
