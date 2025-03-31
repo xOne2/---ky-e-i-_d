b675d9a4557f90fec6c613f53be200980cae1360
Wx <106684512+xOne2@users.noreply.github.com>
2025-04-01 00:22:28 +0700

    Update 239120940.lua

--- 239120940.lua
+++ 239120940.lua
@@ -1,15 +1,5 @@
 local whitelistPak = {
-    { Hwid = "x", Key = "x"{ Hwid = "z", Key = "z"{ Hwid = "asdasdasd", Key = "asdasd"
-         s { Hwid = "sdd", Key = "add" 
-     { Hwid = "1231", Key = "1231"
-    { Hwid = "xnxx", Key = "xnxx"
-    { Hwid = "ffffff", Key = "fffff"
-    { Hwid = "lllllllllll", Key = "lllllllll"
-    { Hwid = "hhhhhhhhhh", Key = "asdasd2"},
-    },
-    },
-    },
-    },
-    },
-    },
-  },},}
+    
+
+
+}
