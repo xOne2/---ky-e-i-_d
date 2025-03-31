1a0a7f6c9989919b18fdccbf78c6025fe03dad38
Wx <106684512+xOne2@users.noreply.github.com>
2025-03-31 21:28:15 +0700

    Update 239120940.lua

--- 239120940.lua
+++ 239120940.lua
@@ -1,5 +1,3 @@
 local whitelistPak = {
     { Hwid = "444", Key = "222", ExpiryDate = math.huge, Permanent = true },
-    { Hwid = "23", Key = "554", ExpiryDate = math.huge, Permanent = true },
-    { Hwid = "", Key = "", ExpiryDate = math.huge, Permanent = true },
 }
