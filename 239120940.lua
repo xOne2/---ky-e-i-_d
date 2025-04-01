9fe218e9b7e2814a0a3282a450b6ad0c68f35f40
Wx <106684512+xOne2@users.noreply.github.com>
2025-04-01 20:06:48 +0700

    Update HWID with Key

--- 239120940.lua
+++ 239120940.lua
@@ -2,5 +2,6 @@ local whitelistPak = {
 
 
      { Key = "8ez8yl3hR2xdpi4y", HWID = "a", ExpiryDate = math.huge, Permanent = true,
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "u", ExpiryDate = math.huge, Permanent = true }
-}
+    { Key = "8ez8yl3hR2xdpi4y", HWID = "u", ExpiryDate = math.huge, Permanent = true,
+    { Key = "8ez8yl3hR2xdpi4y", HWID = "asd", ExpiryDate = math.huge, Permanent = true }
+ }
