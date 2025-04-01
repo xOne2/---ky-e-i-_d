908df42d17f121f750cb64ebb234fa9484b8f965
Wx <106684512+xOne2@users.noreply.github.com>
2025-04-01 22:19:26 +0700

    Update 239120940.lua

--- 239120940.lua
+++ 239120940.lua
@@ -1,12 +1,3 @@
 local whitelistPak = {
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "a"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "a"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "HWID1"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "1"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "HWID 1"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "1"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "s"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "a"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "kk"},
-    { Key = "8ez8yl3hR2xdpi4y", HWID = "tt"},
+    
 }
