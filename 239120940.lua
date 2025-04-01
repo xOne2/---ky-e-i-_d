5b276835914d1a055a63bbf7024cf3d98ca178bd
Wx <106684512+xOne2@users.noreply.github.com>
2025-04-01 14:32:27 +0700

    Update HWID keys

--- 239120940.lua
+++ 239120940.lua
@@ -1,5 +1,6 @@
 local whitelistPak = {
-    { Hwid = "1119509280480038972", Key = "FUByyhM9bB7OATEI", ExpiryDate = math.huge, Permanent = true },
+    { Hwid = "1119509280480038972", Key = "IKGQAoekgSZyLEsh", ExpiryDate = math.huge, Permanent = true },
+  { Hwid = "1119509280480038972", Key = "FUByyhM9bB7OATEI", ExpiryDate = math.huge, Permanent = true},
   { Hwid = "1119509280480038972", Key = "v5IlE35ce8tqSUe1", ExpiryDate = math.huge, Permanent = true},
   { Hwid = "1119509280480038972", Key = "Y78EXD8ou0BaWN6R", ExpiryDate = math.huge, Permanent = true},
   { Hwid = "q", Key = "q", ExpiryDate = math.huge, Permanent = true},
